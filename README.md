9719fd32d59dea0ac43c72328b3f76aa2395ab0a
김동영/서비스플랫폼개발팀/EP <dongyoung.kim@nhnenter.com>
2024-05-11 16:29:28 +0900

    fix: 도커 스크립트 수정, 리드미 프로메테우스 설명 추가

--- README.md
+++ README.md
@@ -1,12 +1,33 @@
 # 샘플용 자바11 스프링부트2 프로젝트
-> port: 8080
+> server port: 8080
+> actuator port: 8081  
 
-## build
+## 1. 로컬 빌드 - 실행
+### 1. build
 ```shell
 ./gradlew clean build
 ```
 
-## run jar
+### 2. run jar
 ```shell
 java -jar build/libs/sample-spring-boot-2-java-gradle-0.0.1-SNAPSHOT.jar
+```
+
+## 2. 도커 빌드 - 실행
+### 1. docker build
+```shell
+docker build -t sample-spring-boot-2-java-gradle .
+```
+
+### 2. docker run
+```shell
+docker run -d -p 8080:8080 -p 8081:8081 sample-spring-boot-2-java-gradle
+```
+
+## 99. 참고
+### 1. 프로메테우스 - 그라파나(via. docker-compose)
+[모니터링 도커 컴포즈](.docker/monitoring/README.md)
+
+### 2. DB(via. docker-compose)
+[DB 도커 컴포즈](.docker/db/README.md)
 ```
--- runScript.sh
+++ runScript.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 BUILD_NAME=sample-springboot2-jdk11-gradle
-EXPOSE_PORT=8082
+SERVER_PORT=8080
+ACTUATOR_PORT=8081
 
 docker rmi $BUILD_NAME
 docker build -t $BUILD_NAME .
 
 # foreground -> script exit = docker process stop
-docker run --rm -p $EXPOSE_PORT:8080 --name $BUILD_NAME $BUILD_NAME
+docker run --rm -p $SERVER_PORT:8080 -p $ACTUATOR_PORT:8081 --name $BUILD_NAME $BUILD_NAME
 
 # for docker data remaining local
-#docker run --rm -p $EXPOSE_PORT:8080 --name $BUILD_NAME -v ./:/app $BUILD_NAME
+#docker run --rm -p $SERVER_PORT:8080 -p $ACTUATOR_PORT:8081 --name $BUILD_NAME -v ./:/app $BUILD_NAME
 
 # background -> must docker stop manually for process exit
-# docker run -d --rm -p $EXPOSE_PORT:8080 --name $BUILD_NAME $BUILD_NAME
+# docker run -d --rm -p $SERVER_PORT:8080 -p $ACTUATOR_PORT:8081 --name $BUILD_NAME $BUILD_NAME
 # docker stop $BUILD_NAME
